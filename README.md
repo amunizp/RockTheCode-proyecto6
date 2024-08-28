d0be3a6738bcf8c2b94c9b45797f3e2022f609ee
Andres Muniz Piniella <amp-git@c4ad.eu>
2024-08-28 18:35:41 +0100

    hasta aqui funciona la base

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # RockTheCode-proyecto6
+
 An issue tracker for Neighbours.
+
+## The problem
+
+Within blocks of flats there are always a group of volunteers that like to look after the building and other neighbors.
+
+The problem is that the historical issues are lost between neighbours and some other neighbours might want to pickup where a previous neighbour picked up.
+
+## The proposed solution: Issue tracker
+
+A neighbour raises an issue and describes it.
+
+authour: string
+Description: string
+ref flat Optional
+ref issueUpdate optional
+
+As hinted above The issue has a flat location.
+
+number : int
+court : string
+
+Another neighbour might want to update an existing issue.
+
+authour: string
+descrition: string
+
+## Reach goal
+
+Give a list of the issues attached to a particular flat.
---
+++ index.js
@@ -0,0 +1,24 @@
+console.log('esto arrancó')
+// configurar que pueda acceder a las variables del .env, se suele realizar lo primero de todo
+require('dotenv').config()
+
+// me traigo el módulo de express para poder crear el servidor
+const express = require('express')
+// me traigo la función de conexión con la bbdd que hemos creado previamente
+const { connectDB } = require('./src/config/db')
+// ejecutamos express y lo guardamos en una variable, en mi caso me gusta llamarle app porque así viene en la documentación de la librería
+const app = express()
+
+connectDB()
+
+// req.body de formato json
+app.use(express.json())
+
+app.use('*', (req, res, next) => {
+  return res.status(404).json('Route not found')
+})
+
+// ponemos a escuchar a nuestro servidor
+app.listen(3000, () => {
+  console.log('http://localhost:3000')
+})
